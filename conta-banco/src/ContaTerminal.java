f855dc8f2b32adba07ce8c485d8e768a65e70700
Bosquim <joaobosco1152@gmail.com>
2024-05-14 16:08:43 -0300

    novo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -5,15 +5,17 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in, "UTF-8").useLocale(Locale.US);
         System.out.println("Bem vindo ao Sistema de Banco!");
-        System.out.println("qual é o seu nome?");
+        System.out.println("qual é o seu primeiro nome?");
         String nome = scanner.next();
-        System.out.println(nome +" digite o numero da conta:");
+        System.out.println(nome + ", qual é o seu sobrenome?");
+        String sobrenome = scanner.next();
+        System.out.println(nome + sobrenome + ", digite o numero da conta:");
         int numero = scanner.nextInt();
-        System.out.println(nome +" digite a sua agencia");
+        System.out.println(nome + sobrenome + ", digite a sua agencia");
         int agencia = scanner.nextInt();
-        System.out.println(nome + " qual é o seu saldo?");
+        System.out.println(nome + sobrenome +  ", qual é o seu saldo?");
         double saldo = scanner.nextDouble(); 
 
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta:" + numero + " e seu saldo: " + saldo + "R$ já disponivel para saque.");
+        System.out.println("Olá " + nome + sobrenome +  ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta:" + numero + " e seu saldo: " + saldo + "R$ já disponivel para saque.");
     }
 }
